635a8e51e8019d52280d4c7f04aa2360c7d20efc
Alex <alexkelly97@hotmail.co.uk>
2026-06-30 20:46:04 +0100

    update stepping analysis to use hit manager, no longer depends on event analysis, and slight cleanup

--- final/include/SteppingAnalysis.hh
+++ final/include/SteppingAnalysis.hh
@@ -2,17 +2,19 @@
 #define MyStepAnalysis_HH
 
 // User classes
-#include "EventAnalysis.hh"
+// #include "EventAnalysis.hh"
 #include "AnalysisRegistry.hh"
+#include "ProgramState.hh" // .....
 
 // G4 lib
 #include "G4Step.hh"
-#include "G4GenericAnalysisManager.hh"
 
 // Forward declarations
 // struct StepDetectionNtupleIDs;
 // struct StepBoundaryAbsorbNtupleIDs;
 // struct StepBulkAbsorbNtupleIDs;
+class G4GenericAnalysisManager;
+class HitManager;
 
 /*
  * Handles writing output data to ntuples
@@ -21,19 +23,19 @@
 class SteppingAnalysis : public RegistryListener { // TEST
     public:
         // Constructor
-        SteppingAnalysis();
+        SteppingAnalysis(HitManager* hitManager);
         
         // Destructor
         ~SteppingAnalysis() override = default;
         
         // ...
-        void HandleBulkAbsorb(G4Track const* track, EventAnalysis const* fEventAnalysis);
+        void HandleBulkAbsorb(G4Track const* track, HitManager const* fHitManager) const;
         
         // ...
-        void HandleDetection(G4StepPoint const* endPoint, G4Track const* track, EventAnalysis const* fEventAnalysis);
+        void HandleDetection(G4StepPoint const* endPoint, G4Track const* track, HitManager const* fHitManager) const;
         
         // ...
-        void HandleBoundaryAbsorb(G4StepPoint const* endPoint);
+        void HandleBoundaryAbsorb(G4StepPoint const* endPoint) const;
         
         // TEST
         void UpdateRegistryCache() override;
@@ -43,9 +45,17 @@ class SteppingAnalysis : public RegistryListener { // TEST
         G4GenericAnalysisManager* fAnalysisManager = nullptr;
         
         // ...
+        HitManager* fHitManager = nullptr;
+        
+        // ...
         StepDetectionNtupleIDs const* fStepDetectionNtupleIDs = nullptr;
         StepBoundaryAbsorbNtupleIDs const* fStepBoundaryAbsorbNtupleIDs = nullptr;
         StepBulkAbsorbNtupleIDs const* fStepBulkAbsorbNtupleIDs = nullptr;
+        
+        // ....
+        StepDetectionFlags const* fStepDetectionFlags = nullptr;
+        StepBoundaryAbsorbFlags const* fStepBoundaryAbsorbFlags = nullptr;
+        StepBulkAbsorbFlags const* fStepBulkAbsorbFlags = nullptr;
 };
 
 #endif
--- final/src/SteppingAnalysis.cc
+++ final/src/SteppingAnalysis.cc
@@ -2,6 +2,7 @@
 #include "SteppingAnalysis.hh"
 #include "ProgramState.hh"
 // #include "AnalysisRegistry.hh"
+#include "HitManager.hh"
 
 // G4 lib
 #include "G4AnalysisManager.hh"
@@ -21,12 +22,12 @@
  * 
  * NOTE: Owned by SteppingAction
  */
-SteppingAnalysis::SteppingAnalysis() {
+SteppingAnalysis::SteppingAnalysis(HitManager* hitManager) : fHitManager(hitManager) {
     // Cache pointer to analysis manager singleton
     fAnalysisManager = G4AnalysisManager::Instance();
     
     // ...
-    ProgramState& instance = ProgramState::GetInstance(); // TODO: Class property
+    // ProgramState& instance = ProgramState::GetInstance(); // TODO: Class property
     
     // ...
     AnalysisRegistry& registry = AnalysisRegistry::GetInstance();
@@ -52,15 +53,16 @@ void SteppingAnalysis::UpdateRegistryCache() {
     // G4cout << "\n\n>>>>> STEPPING ANALYSIS LISTENER\n\n" << G4endl;
     
     // ...
-    AnalysisRegistry& registry = AnalysisRegistry::GetInstance();
+    AnalysisRegistry const& registry = AnalysisRegistry::GetInstance();
     
-    // ...
-    const NtupleIDs& ntupleIDs = registry.ReadNtupleIDs();
+    // Get readonly reference to ntuple indices object
+    NtupleIDs const& ntupleIDs = registry.ReadNtupleIDs();
     
     // ...
     fStepDetectionNtupleIDs = &(ntupleIDs.fStepDetectionNtupleIDs);
     fStepBoundaryAbsorbNtupleIDs = &(ntupleIDs.fStepBoundaryAbsorbNtupleIDs);
     fStepBulkAbsorbNtupleIDs = &(ntupleIDs.fStepBulkAbsorbNtupleIDs);
+    // NOTE: Cache a pointer to the indices, dont copy the data itself
 }
 
 /*
@@ -70,23 +72,10 @@ void SteppingAnalysis::UpdateRegistryCache() {
  * 
  * NOTE: Takes readonly pointers (pointer to const)
  */
-void SteppingAnalysis::HandleBulkAbsorb(G4Track const* track, EventAnalysis const* fEventAnalysis) {
+void SteppingAnalysis::HandleBulkAbsorb(G4Track const* track, HitManager const* fHitManager) const {
     // ..
     // std::string volume = endPoint->GetTouchable()->GetVolume()->GetName();
     // G4cout << "BULK ABSORPTION IN MEDIUM: " << endPoint->GetTouchable()->GetVolume()->GetName() << G4endl; // "Scintillator"
-    
-//     // Get distance travelled by photon before bulk absorption
-//     G4double const distance = track->GetTrackLength();
-//     fAnalysisManager->FillNtupleDColumn(4, 0, distance); // id = 4, column = 0, value = distance travelled
-//     fAnalysisManager->AddNtupleRow(4); // finish row for Ntuple id = 4
-//     // G4cout << "Distance Travelled Before Bulk Absorption: " << distance << " mm" << G4endl;
-//     
-//     // Retrieve number of reflections
-//     // G4int const numReflections = userTrackInfo->GetReflections();
-//     G4int const photonIdx = track->GetTrackID();
-//     G4int const numReflections = fEventAnalysis->GetReflections(photonIdx);
-//     fAnalysisManager->FillNtupleIColumn(7, 0, numReflections); // ...
-//     fAnalysisManager->AddNtupleRow(7); // ...
     
     // Get distance travelled by photon before bulk absorption
     G4double const distance = track->GetTrackLength();
@@ -99,9 +88,8 @@ void SteppingAnalysis::HandleBulkAbsorb(G4Track const* track, EventAnalysis cons
     // G4cout << "Distance Travelled Before Bulk Absorption: " << distance << " mm" << G4endl;
     
     // Retrieve number of reflections
-    // G4int const numReflections = userTrackInfo->GetReflections();
     G4int const photonIdx = track->GetTrackID();
-    G4int const numReflections = fEventAnalysis->GetReflections(photonIdx);
+    G4int const numReflections = fHitManager->GetReflections(photonIdx);
     fAnalysisManager->FillNtupleIColumn(
         fStepBulkAbsorbNtupleIDs->fBulkAbsorbReflectionsNtuple.fNtupleID, 
         fStepBulkAbsorbNtupleIDs->fBulkAbsorbReflectionsNtuple.fColumnID, 
@@ -126,7 +114,7 @@ void SteppingAnalysis::HandleBulkAbsorb(G4Track const* track, EventAnalysis cons
  * }
  * ... etc
  */
-void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track const* track, EventAnalysis const* fEventAnalysis) {
+void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track const* track, HitManager const* fHitManager) const {
     // TODO: May be worth double checking the boundary is the photocathode
     // ...although since its only one with efficiency vector, it will be
     
@@ -142,15 +130,6 @@ void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track cons
     double const x = detectionPosition[0];
     double const y = detectionPosition[1];
     double const z = detectionPosition[2];
-    
-//     // Store this data in the nTuples (create a few rows) (IColumn = int, DColumn = double)
-//     fAnalysisManager->FillNtupleDColumn(0, 0, x); // NtupleID = 0, 0th column, x
-//     fAnalysisManager->FillNtupleDColumn(0, 1, y); // NtupleID = 0, 1st column, y
-//     fAnalysisManager->FillNtupleDColumn(0, 2, z); // NtupleID = 0, 2nd column, z
-//     // NOTE: Takes tuple ID (0 as we only made one), column number in this row, and the entry
-//     
-//     // Mark this row as complete (for Ntuple with passed ID)
-//     fAnalysisManager->AddNtupleRow(0); // Ntuple ID = 0
     
     // TODO: Maybe cache/reuse a little bit, might actually reduce chances of human error too tbh
     // \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
@@ -194,25 +173,6 @@ void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track cons
     // again much like photon counts
     // TEST TEST TEST
     
-//     // Get distance travelled by photon before detection (from creation to absorption in PC)
-//     G4double const distance = track->GetTrackLength();
-//     fAnalysisManager->FillNtupleDColumn(3, 0, distance); // NtupleID = 3, column = 0, val = distance
-//     // analysisManager->AddNtupleRow(3); // finish row for NtupleID = 3 // NOTE: OMIT THIS IF USING SAME NTUPLE FOR ALL TRACK DATA
-//     // G4cout << "Distance Travelled Before Detection: " << distance << " mm" << G4endl;
-//     
-//     // Get time of flight information (local time gives time since photon birth until now, i.e. birth to detection)
-//     G4double const time = track->GetLocalTime();
-//     fAnalysisManager->FillNtupleDColumn(3, 1, time); // NtupleID = 3, column = 1, val = time
-//     fAnalysisManager->AddNtupleRow(3); // Finish row for NtupleID = 3 // NOTE: NOW THAT ALL TRACK DATA WRITTEN, ADD THE ROW
-//     // NOTE: This method loses relative timing between different photons if they 
-//     // were created at different points along the primary particles track
-//     
-//     // Retrieve number of reflections
-//     G4int const photonIdx = track->GetTrackID();
-//     G4int const numReflections = fEventAnalysis->GetReflections(photonIdx);
-//     fAnalysisManager->FillNtupleIColumn(6, 0, numReflections); // TODO: Maybe clump all "detection" branches together in one ntuple
-//     fAnalysisManager->AddNtupleRow(6);
-    
     // Get distance travelled by photon before detection (from creation to absorption in PC)
     G4double const distance = track->GetTrackLength();
     fAnalysisManager->FillNtupleDColumn(
@@ -236,7 +196,7 @@ void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track cons
     
     // Retrieve number of reflections
     G4int const photonIdx = track->GetTrackID();
-    G4int const numReflections = fEventAnalysis->GetReflections(photonIdx);
+    G4int const numReflections = fHitManager->GetReflections(photonIdx);
     fAnalysisManager->FillNtupleIColumn(
         fStepDetectionNtupleIDs->fDetectionReflectionsNtuple.fNtupleID,
         fStepDetectionNtupleIDs->fDetectionReflectionsNtuple.fColumnID,
@@ -266,7 +226,7 @@ void SteppingAnalysis::HandleDetection(G4StepPoint const* endPoint, G4Track cons
  * 
  * NOTE: Takes readonly pointer (pointer to const)
  */
-void SteppingAnalysis::HandleBoundaryAbsorb(G4StepPoint const* endPoint) {
+void SteppingAnalysis::HandleBoundaryAbsorb(G4StepPoint const* endPoint) const {
     // TODO: Can get absorption volume via same process as above,
     // counts absorptions in crystal vs reflector, vs photocathode
     
@@ -275,15 +235,6 @@ void SteppingAnalysis::HandleBoundaryAbsorb(G4StepPoint const* endPoint) {
     double const x = absorptionPosition[0];
     double const y = absorptionPosition[1];
     double const z = absorptionPosition[2];
-    
-//     // Store this data in the nTuples (create a few rows) (IColumn = int, DColumn = double)
-//     fAnalysisManager->FillNtupleDColumn(1, 0, x); // NtupleID = 1, 3rd column, x
-//     fAnalysisManager->FillNtupleDColumn(1, 1, y); // NtupleID = 1, 4th column, y
-//     fAnalysisManager->FillNtupleDColumn(1, 2, z); // NtupleID = 1, 5th column, z
-//     // NOTE: Takes tuple ID (0 as we only made one), column number in this row, and the entry
-//     
-//     // Mark this row as complete (for Ntuple with passed ID)
-//     fAnalysisManager->AddNtupleRow(1); // Ntuple ID = 1
     
     // Store this data in the nTuples (create a few rows) (IColumn = int, DColumn = double)
     fAnalysisManager->FillNtupleDColumn(
