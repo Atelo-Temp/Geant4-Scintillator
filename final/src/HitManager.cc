b8b03d12778e1a9c18081603c03e8798e2772635
Alex <alexkelly97@hotmail.co.uk>
2026-06-30 19:44:29 +0100

    extracted hit tracking out of event analysis (so both event analysis and stepping analysis primary responsibility is to just write to ntuples), better separation of concerns imo, and stepping analysis will no longer need pointer to event analysis, both will just have hit manager pointer

---
+++ final/src/HitManager.cc
@@ -0,0 +1,89 @@
+// User lib
+#include "HitManager.hh"
+
+/*
+ * Constructor
+ */
+HitManager::HitManager() {
+    // Increase allocation for reflection vector map
+    fReflectionMap.resize(40960);
+}
+
+/*
+ * Reset counters between events
+ */
+void HitManager::ResetCounters() {
+    // Set integers to zero
+    fTotalPhotons = 0;
+    fDetectedPhotons = 0;
+    fAbsorbedPhotons = 0;
+    fBulkAbsorb = 0;
+    fLostPhotons = 0; // NoRINDEX
+    
+    // Set all indices in reflection map to zero values
+    // fReflectionMap.assign(fReflectionMap.size(), 0);
+    std::fill(fReflectionMap.begin(), fReflectionMap.end(), 0);
+}
+
+/*
+ * Increment optical photons generated
+ */
+void HitManager::CountPhoton() {
+    fTotalPhotons += 1;
+}
+
+/*
+ * Increment optical photons detected (at photocathode)
+ */
+void HitManager::CountDetectedPhoton() {
+    fDetectedPhotons += 1;
+}
+
+/*
+ * Increment optical photons absorbed (at a boundary)
+ */
+void HitManager::CountAbsorbedPhoton() {
+    fAbsorbedPhotons += 1;
+}
+
+/*
+ * Increment optical photons lost (due to no RINDEX etc)
+ */
+void HitManager::CountLostPhoton() {
+    fLostPhotons += 1;
+}
+// void HitManager::CountLostPhoton(std::string medium) { fLostPhotons += 1; } // TODO: Add medium where each of these things occured (same for absorption, etc)
+
+/*
+ * Increment optical photons absorbed (in medium)
+ */
+void HitManager::CountBulkAbsorption() {
+    fBulkAbsorb += 1;
+}
+
+/*
+ * Increment reflection counter for specified photon
+ * 
+ * NOTE: This is for any type of reflection:
+ * - FresnelReflection
+ * - TotalInternalReflection
+ * - LambertianReflection
+ * - LobeReflection
+ * - SpikeReflection
+ * - BackScattering
+ * 
+ * NOTE: Will increase size of vector if passed photonIdx exceeds capacity
+ */
+void HitManager::CountReflection(G4int photonIdx) {
+    if (photonIdx >= fReflectionMap.size()) fReflectionMap.resize(photonIdx + 2048, 0);
+    fReflectionMap[photonIdx]++;
+}
+
+/*
+ * Retrieve total number of reflections for specified photon
+ * 
+ * NOTE: Doesnt alter class data (readonly), so const method
+ */
+G4int HitManager::GetReflections(G4int photonIdx) const {
+    return fReflectionMap[photonIdx];
+}
